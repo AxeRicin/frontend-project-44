91c3c91d583728a38d03214d5afeefc632ef3d3e
AxeRicin <AxeRicin@yandex.ru>
2023-04-23 23:38:13 +0300

    Refactoring brain-even

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -6,24 +6,25 @@ const maxNum = 100;
 
 const getRandomNumber = () => Math.floor(Math.random() * maxNum + 1);
 
+const checkingAnswerCorrect = (number) => {
+  if (number % 2 === 0) {
+    return 'yes';
+  }
+  return 'no';
+};
+
 const evenGame = (name = 'Bill') => {
   console.log('Answer "yes" if the number is even, otherwise answer "no".');
   for (let i = 0; i < 3; i += 1) {
     const number = getRandomNumber();
     console.log(`Question: ${number}`);
     const answer = askQuestion('Your answer: ');
+    const answerCorrect = checkingAnswerCorrect(number);
 
-    if (number % 2 === 0) {
-      if (answer === 'yes') {
-        print('Correct!');
-      } else {
-        print("'no' is wrong answer ;(. Correct answer was 'yes'.");
-        return print(`Let's try again, ${name}`);
-      }
-    } else if (answer === 'no') {
+    if (answer === answerCorrect) {
       print('Correct!');
     } else {
-      print("'yes' is wrong answer ;(. Correct answer was 'no'.");
+      print(`'${answer}' is wrong answer ;(. Correct answer was '${answerCorrect}'.`);
       return print(`Let's try again, ${name}`);
     }
   }
