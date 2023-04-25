3cb679d5b7a7a57b15f39e878b3325e2d62699d5
AxeRicin <AxeRicin@yandex.ru>
2023-04-25 18:22:25 +0300

    Add gameGcd

---
+++ bin/brain-gcd.js
@@ -0,0 +1,5 @@
+#!/usr/bin/env node
+
+import dcdGame from '../src/games/dcd.js';
+
+dcdGame();
---
+++ src/games/dcd.js
@@ -0,0 +1,49 @@
+import * as cli from '../cli.js';
+import engin, { getRandomNumber } from '../index.js';
+
+const getExample = () => {
+  const num1 = getRandomNumber();
+  const num2 = getRandomNumber();
+
+  return `${num1} ${num2}`;
+};
+
+const findMaxDivisor = (coll1, coll2) => {
+  let acc = 0;
+
+  for (let i = 0; i < coll1.length; i += 1) {
+    if (coll2.indexOf(coll1[i]) !== -1) {
+      acc = coll1[i];
+    }
+  }
+
+  return `${acc}`;
+};
+
+const getDivisorsOperand = (operand) => {
+  const collDevisorOperand = [];
+  for (let i = 0; i <= operand; i += 1) {
+    if (operand % i === 0) {
+      collDevisorOperand.push(i);
+    }
+  }
+  return collDevisorOperand;
+};
+
+const getTrueAnswer = (example) => {
+  const [operand1, operand2] = example.split(' ');
+  const collDevisorOperand1 = getDivisorsOperand(operand1);
+  const collDevisorOperand2 = getDivisorsOperand(operand2);
+
+  return findMaxDivisor(collDevisorOperand1, collDevisorOperand2);
+};
+
+const dcdGame = () => {
+  const name = cli.seyHello();
+  cli.print('What is the result of the expression?');
+  engin(name, getExample, getTrueAnswer);
+};
+
+dcdGame();
+
+export default dcdGame;
