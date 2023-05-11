e3a87e0ee084e542f95d38a45cb6117a88c9ff12
AxeRicin <AxeRicin@yandex.ru>
2023-05-11 06:15:15 +0300

    Update prime

--- src/games/prime.js
+++ src/games/prime.js
@@ -1,26 +1,26 @@
-import engin from '../index.js';
+import startPrimeGame from '../index.js';
 import getRandomNumber from '../randomNumber.js';
 
-const isCorrectAnswer = (num) => {
-  let answer = num === 1 ? 'no' : 'yes';
+const isPrimeNumber = (num) => {
+  if (num === 0 || num === 1) {
+    return false;
+  }
 
   for (let i = 2; i < num; i += 1) {
     if (num % i === 0) {
-      answer = 'no';
-      break;
+      return false;
     }
   }
-  return answer;
+  return true;
 };
 
 const getRoundData = () => {
   const question = getRandomNumber();
+  const correctAnswer = isPrimeNumber(question) ? 'yes' : 'no';
 
-  return [question, isCorrectAnswer(question)];
+  return [question, correctAnswer];
 };
 
-const primeGame = () => {
-  engin('Answer "yes" if given number is prime. Otherwise answer "no".', getRoundData);
+export default () => {
+  startPrimeGame('Answer "yes" if given number is prime. Otherwise answer "no".', getRoundData);
 };
-
-export default primeGame;
