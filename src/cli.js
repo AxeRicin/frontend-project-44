6ad9705428b6cdeb5c33f364dc0149fb5ae9d0ae
AxeRicin <AxeRicin@yandex.ru>
2023-05-11 06:14:26 +0300

    Fix cli

--- src/cli.js
+++ src/cli.js
@@ -1,15 +1,9 @@
 import readlineSync from 'readline-sync';
 
-const askQuestion = (question) => {
-  const answer = readlineSync.question(question);
-  return answer;
-};
-
 const seyHello = () => {
   console.log('Welcome to the Brain Games!');
-  const name = askQuestion('May I have your name? ');
+  const name = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${name}!`);
-  return name;
 };
 
-export { askQuestion, seyHello };
+export default seyHello;
