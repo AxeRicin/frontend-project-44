aa3526895575bae309714052f5f5b29411d64715
AxeRicin <AxeRicin@yandex.ru>
2023-05-17 20:17:15 +0300

    Fix calc

--- src/games/calc.js
+++ src/games/calc.js
@@ -17,16 +17,16 @@ const getCorrectAnswer = (operand1, operator, operand2) => {
       return operand1 * operand2;
 
     default:
-      throw new Error(`Unknown operand or operator: '${operand1}, ${operator}, ${operand2}'!`);
+      throw new Error(`Unknown operator: ${operator}!`);
   }
 };
 
 const getRoundData = () => {
   const operand1 = getRandomNumber(1, 50);
   const operand2 = getRandomNumber(1, 50);
-  const operator = getOperator(getRandomNumber(0, 2));
+  const operator = getOperator(Math.floor(Math.random() * operators.length));
   const question = `${operand1} ${operator} ${operand2}`;
-  const correctAnswer = `${getCorrectAnswer(operand1, operator, operand2)}`;
+  const correctAnswer = String(getCorrectAnswer(operand1, operator, operand2));
 
   return [question, correctAnswer];
 };
